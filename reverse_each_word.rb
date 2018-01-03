8e5b2e5d4f8aaafc85df9b9ea34f0da6b5b2df58
Larissa S <larissafschiavo@gmail.com>
2018-01-03 18:30:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(str)
   spl_str = str.split(" ")
-  ans = []
-  spl_str.collect {|x| x.reverse }
-  spl_str.join(" ")
+  ans = spl_str.collect {|x| x.reverse! }
+  ans.join(" ")
 end
